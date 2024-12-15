234061b10f3f28bc0c662706a4cfc0413234f517
Federica Zanca <federica.zanca@expleoitalia.com>
2024-12-15 13:00:19 +0100

    lists and persons with errors

--- backend/main.py
+++ backend/main.py
@@ -23,21 +23,22 @@ def read_root():
 
 class Persona(BaseModel):
     nome:str
+    email:str
 
-@santapp.put("/names")
-def add_persona(persona:Persona):
-    params={
-        "host":"postgres",
-        "port":"5432",
-        "dbname":"santa",
-        "user":"admin",
-        "password":"password"    
-    }
+# @santapp.put("/names")
+# def add_persona(persona:Persona):
+#     params={
+#         "host":"postgres",
+#         "port":"5432",
+#         "dbname":"santa",
+#         "user":"admin",
+#         "password":"password"    
+#     }
 
-    with psycopg2.connect(**params) as conn:
-        with conn.cursor() as cur:
-            cur.execute("INSERT INTO elenco (NOME) VALUES (%s)", (persona.nome,))
-            conn.commit()
+#     with psycopg2.connect(**params) as conn:
+#         with conn.cursor() as cur:
+#             cur.execute("INSERT INTO elenco (NOME) VALUES (%s)", (persona.nome,))
+#             conn.commit()
 
 
 @santapp.get("/extract")
@@ -51,18 +52,17 @@ def extract_random_name():
     }
     with psycopg2.connect(**params) as conn:
         with conn.cursor() as cur:
-            # Query per estrarre un nome casuale
             cur.execute("SELECT NOME FROM elenco ORDER BY RANDOM() LIMIT 1")
             result = cur.fetchone()
     
     return {"random_name": result[0]}
 
 
-class Titolo(BaseModel):
+class Lista(BaseModel):
     titolo:str  
 
 @santapp.post("/lista")
-def make_list(titolo:Titolo):
+def make_list(lista:Lista):
     params={
         "host":"postgres",
         "port":"5432",
@@ -73,5 +73,58 @@ def make_list(titolo:Titolo):
 
     with psycopg2.connect(**params) as conn:
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO elenco (NOME) VALUES (%s)", (persona.nome,))
+            cur.execute("Insert INTO elenco (nome) VALUES (%s) ;", (lista.titolo,))
             conn.commit()
+            cur.execute("SELECT id FROM elenco WHERE nome = %s;", (lista.titolo,))
+            id = cur.fetchone()[0]
+            
+    return {"id":id}
+
+@santapp.post("/lista/:id")
+def add_persona(persona:Persona, id):
+    params={
+        "host":"postgres",
+        "port":"5432",
+        "dbname":"santa",
+        "user":"admin",
+        "password":"password"    
+    }
+
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cur:
+            cur.execute("FROM elenco SELECT id WHERE id = %s;", (id,))
+            cur.execute("CREATE TABLE IF NOT EXISTS persone (id SERIAL PRIMARY KEY, lista_id INT, nome TEXT, email TEXT)")
+            cur.execute("INSERT INTO persone (lista_id, nome, email) VALUES %s %s;", (id, persona.nome, persona.email,))
+            conn.commit()
+
+@santapp.get("/lista")
+def show_list():
+    params = {
+        "host": "postgres",
+        "port": "5432",
+        "dbname": "santa",
+        "user": "admin",
+        "password": "password"
+    }
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, nome FROM elenco")
+            result = cur.fetchall()
+    
+    return result
+
+@santapp.get("/lista/{id}")
+def show_persone():
+    params = {
+        "host": "postgres",
+        "port": "5432",
+        "dbname": "santa",
+        "user": "admin",
+        "password": "password"
+    }
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT nome, email FROM persone WHERE lista_id = %s;", (id,))
+            persone = cur.fetchall()
+    
+    return persone
